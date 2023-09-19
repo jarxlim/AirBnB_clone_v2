0699feb2047c28e95d2f39114e8c8c2a5d1dc9bc
Chike Chima <chimachike1@gmail.com>
2023-09-19 08:20:05 -0700

    update models

--- models/user.py
+++ models/user.py
@@ -16,7 +16,6 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         _password = Column(String(128), nullable=False)
@@ -26,22 +25,3 @@ class User(BaseModel, Base):
                           cascade='all, delete-orphan')
         reviews = relationship('Review', backref='user',
                            cascade='all, delete-orphan')
-
-    else:
-        email = ""
-        _password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
-
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, pwd):
-        """hashing password values"""
-        self._password = pwd
