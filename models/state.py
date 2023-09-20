c033f77f9aac208eb92975612c65f5518bbebe69
Chike Chima <chimachike1@gmail.com>
2023-09-20 09:42:50 -0700

    done

--- models/state.py
+++ models/state.py
@@ -10,26 +10,15 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """Representation of state """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128),
-                      nullable=False)
-        cities = relationship("City", cascade="all, delete",
-                              backref="states")
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
     else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """fs getter attribute that returns City instances"""
-            values_city = models.storage.all("City").values()
-            list_city = []
-            for city in values_city:
-                if city.state_id == self.id:
-                    list_city.append(city)
-            return list_city
+            """Getter attribute in case of file storage"""
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
--- models/user.py
+++ models/user.py
@@ -16,31 +16,13 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        _password = Column('password', String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                          cascade='all, delete-orphan')
-        reviews = relationship('Review', backref='user',
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='user',
+            cascade='all, delete-orphan')
+    reviews = relationship('Review', backref='user',
                            cascade='all, delete-orphan')
-        else:
-        email = ""
-        _password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
-
-    @property
-    def password(self):
-        return self._password
-
-    @password.setter
-    def password(self, pwd):
-        """hashing _password values"""
-        self._password = pwd
